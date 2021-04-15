e265c6903137b7790eebeb2e60d6f859f1c9a21d
Mark Boyle <markboyle19@gmail.com>
2021-04-16 09:31:10 +1000

    Created method that selects a card

--- lib/round.rb
+++ lib/round.rb
@@ -5,5 +5,17 @@ class Round
         @round_number = 1
         @pot = 0
         @bet = 0
+        @common_cards = []
+        @used_hearts = []
+        @used_diamonds = []
+        @used_clubs = []
+        @used_spades = []
+    end
+
+    def select_card
+        suit = ['heart', 'diamond', 'club', 'spade']
+        card = rand(2..14)
+        selected_card = {suit: suit.sample, card: card}
+        puts selected_card
     end
 end
---
+++ poker.rb
@@ -0,0 +1,7 @@
+require_relative('./lib/round')
+require_relative('./lib/player')
+
+player = Player.new
+round = Round.new
+
+puts round.select_card
