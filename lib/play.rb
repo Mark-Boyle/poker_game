afa8e2a6a0de48fda3b6915f16b2f8d6dc19a059
Mark Boyle <markboyle19@gmail.com>
2021-04-23 17:41:53 +1000

    Can analyse each hand and determine which is the best

--- lib/play.rb
+++ lib/play.rb
@@ -5,35 +5,34 @@ class Play
   attr_accessor :player1, :opponet1, :opponet2, :opponet3
 
     def initialize
-        @player1 = Player.new('Human')
-        @opponent1 = Player.new('Computer')
-        @opponent2 = Player.new('Computer')
-        @opponent3 = Player.new('Computer')
+        @player1 = Player.new('Human', 'Mark')
+        @opponent1 = Player.new('Computer', 'Opponent 1')
+        @opponent2 = Player.new('Computer', 'Opponent 2')
+        @opponent3 = Player.new('Computer', 'Opponent 3')
         @round = Round.new
     end
 
     def run
         deal_hand
+        @player1.check_holding_cards
+        @opponent1.check_holding_cards
+        @opponent2.check_holding_cards
+        @opponent3.check_holding_cards
+        puts @player1.winning_combination[0]
         deal_flop
         deal_turn
         deal_river
         @round.display_common_cards
-        # @player1.check_for_pairs
-        # @opponent1.check_for_pairs
-        # @opponent2.check_for_pairs
-        # @opponent3.check_for_pairs
-        # @player1.check_for_three_of_a_kind
-        # @opponent1.check_for_three_of_a_kind
-        # @opponent2.check_for_three_of_a_kind
-        # @opponent3.check_for_three_of_a_kind
-        # @player1.check_for_full_house
-        # @opponent1.check_for_full_house
-        # @opponent2.check_for_full_house
-        # @opponent3.check_for_full_house
-        @player1.check_for_straight
-        @opponent1.check_for_straight
-        @opponent2.check_for_straight
-        @opponent3.check_for_straight
+        @player1.analyse_hand
+        @opponent1.analyse_hand
+        @opponent2.analyse_hand
+        @opponent3.analyse_hand
+     
+        @player1.display_winning_combination
+        @opponent1.display_winning_combination
+        @opponent2.display_winning_combination
+        @opponent3.display_winning_combination
+        pick_winner
     end
 
     def deal_hand
@@ -87,5 +86,19 @@ class Play
 
     def display_pot
         puts "Pot: #{@round::pot}"
+    end
+
+    def pick_winner
+        
+        winner = [@player1::winning_combinations[0],
+        @opponent1::winning_combinations[0],
+        @opponent2::winning_combinations[0],
+        @opponent3::winning_combinations[0]]
+        # puts @player1::winning_combinations
+        puts winner
+        winner.sort_by!{|k| k[:rank]}
+        puts 'After sorting'
+        puts winner
+        # puts "The winner is #{winner}"
     end
 end
--- lib/player.rb
+++ lib/player.rb
@@ -1,12 +1,15 @@
 class Player
-  attr_accessor :chips, :status, :cards
+  attr_accessor :chips, :status, :cards, :winning_combinations
 
-    def initialize(type)
+    def initialize(type, name)
+        @name = name
         @chips = 250
         @status = 'In play'
         @cards = []
         @hand_ranking = 0
         @type = type
+        @winning_combinations = []
+        @high_card = 0
     end
 
     def display_hand
@@ -23,6 +26,42 @@ class Player
             end
         card_number.sort!.reverse!
     end
+
+    def organise_suits
+        card_suits = []
+        @cards.each_index do | e |
+            card_suits << @cards[e][:suit]
+            end
+        card_suits
+    end
+
+    def analyse_hand
+        check_for_pairs
+        check_for_three_of_a_kind
+        check_for_four_of_a_kind
+        check_for_full_house
+        check_for_straight
+        check_for_flush
+    end
+
+    def display_winning_combination
+        puts ' '
+        @winning_combinations.sort_by!{|k| k[:rank]}
+        puts @winning_combinations[0][:message] unless @winning_combinations.empty?
+    end
+
+    def winning_combination
+        [@winning_combinations[0][:rank]]
+    end
+
+    def check_holding_cards
+        if @cards[0][:card] > @cards[1][:card]
+            @high_card = @cards[0][:card]
+        else
+            @high_card = @cards[1][:card]
+        end
+        @winning_combinations << {rank: 10 + (@high_card * 0.1), message: "You have #{@high_card}-high", name: @name}
+    end
     
     def check_for_pairs
         card_number = organise_card_numbers
@@ -32,11 +71,11 @@ class Player
 
         case b.length 
         when 1
-            puts "You have a pair of #{b.keys[0]}"
+            @winning_combinations << {rank: 9 + (@high_card * 0.1), message: "You have a pair of #{b.keys[0]}", name: @name}
         when 2
-            puts "You have a pair of #{b.keys[0]} and #{b.keys[1]}"
+            @winning_combinations << {rank: 8 + (@high_card * 0.1), message: "You have a pair of #{b.keys[0]} and #{b.keys[1]}", name: @name}
         when 3
-            puts "You have a pair of #{b.keys[0]} and #{b.keys[1]}"
+            @winning_combinations << {rank: 8 + (@high_card * 0.1), message: "You have a pair of #{b.keys[0]} and #{b.keys[1]}", name: @name}
         end
     end
 
@@ -44,25 +83,24 @@ class Player
         card_number = organise_card_numbers
         a = card_number.tally
         b = a.keep_if{|key, value| value == 3}
-        puts "You have three of a kind of #{b.keys[0]}" unless b.empty?
+        @winning_combinations << {rank: 7 + (@high_card * 0.1), message: "You have three of a kind of #{b.keys[0]}", name: @name}  unless b.empty?
     end
 
     def check_for_four_of_a_kind
         card_number = organise_card_numbers
         a = card_number.tally
         b = a.keep_if{|key, value| value == 4}
-        puts "You have four of a kind of #{b.keys[0]}" unless b.empty?
+        @winning_combinations << {rank: 3 + (@high_card * 0.1), message: "You have four of a kind of #{b.keys[0]}", name: @name} unless b.empty?
     end
 
     def check_for_full_house
         card_number = organise_card_numbers
         a = card_number.tally
         b = a.keep_if{|key, value| value == 3 || value == 2}
-        puts "You have a full house!" if b.length > 1 && b.value?(3)
+        @winning_combinations << {rank: 4 + (@high_card * 0.1), message: "You have a full house!", name: @name} if b.length > 1 && b.value?(3)
     end
 
     def check_for_straight
-        puts 'New player'
         card_number = organise_card_numbers
         card_number.each do | num |
             straight = [num]
@@ -70,7 +108,14 @@ class Player
                 straight << num + 1
                   num += 1
             end
-            puts "You have a straight!" if straight.length == 5
+            @winning_combinations << {rank: 6 + (@high_card * 0.1), message: "You have a straight!", name: @name} if straight.length == 5
         end
+    end
+
+    def check_for_flush
+        card_suits = organise_suits
+        a = card_suits.tally 
+        b = a.keep_if{|key, value| value >= 5}
+        @winning_combinations << {rank: 5 + (@high_card * 0.1), message: "You have a flush!", name: @name} unless b.empty?
     end
 end
